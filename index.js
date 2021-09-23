60516d9b4fb4380890d16692fda82f2489b369e4
Abhishek verma <avishekv444@gmail.com>
2021-09-24 00:56:40 +0530

    installed some dependencies for sessions and authenticastion and storing session in db

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 const express=require("express");
+
 const app =express();
 const port=process.env.PORT || 8000 ;
 
+const db=require('./config/mongoose');
+const session=require('express-session'); 
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+
+const MongoStore =require('connect-mongo');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/assets", express.static("./assets"));
+
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(session({
+      name:'employee-review-system',
+      secret:'something',
+      saveUninitialized:false,
+      resave:false,
+      cookie:{
+          maxAge:(1000*60*100)
+      },
+      store:MongoStore.create(
+          {
+              mongoUrl:'mongodb://localhost/employee_review_system',
+              collectionName:'sessions',
+              mongooseConncetion: db,
+              autoRemove: 'disabled'
+          },
+         
+          function(err){
+              console.log(err || 'connect-mongodb setup ok');
+          }
+      )
+    }));
+
+app.use(passport.initialize());
+app.use(passport.session()); 
+app.use(passport.setAuthenticatedUser); 
 
 app.use('/',require('./routes'));
 
