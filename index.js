eb034361bc7e8c84e8468d116bb9efdd67812df7
Abhishek verma <avishekv444@gmail.com>
2021-09-25 00:37:43 +0530

    completed home view

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const MongoStore =require('connect-mongo');
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/assets", express.static("./assets"));
+app.use(express.static('./assets'));
 
 
 app.set("view engine", "ejs");
